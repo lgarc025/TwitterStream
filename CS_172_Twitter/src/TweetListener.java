1cbbe9ace4089d97406d37c35cc4000afa116de2
lgarc025 <lgarc025@ucr.edu>
2016-10-19 20:26:31 -0700

    Got some tweets to save. Need to make sure file size is good.

--- CS_172_Twitter/src/TweetListener.java
+++ CS_172_Twitter/src/TweetListener.java
@@ -1,10 +1,35 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JSeparator;
+
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+
+import twitter4j.GeoLocation;
 import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 
-
 public class TweetListener implements StatusListener { 
+	
+	
+	public List <Tweet> TweetList = new ArrayList<Tweet>();
+	
 	
 	@Override
     public void onScrubGeo(long userId, long upToStatusId) {
@@ -12,8 +37,65 @@ public class TweetListener implements StatusListener {
     }
 	
     @Override
-    public void onStatus(Status status) {
-        System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+    public void onStatus(Status status) 
+    {
+    	/*
+    	while (true){    	
+    	System.out.println(TweetList.size());
+    	if (TweetList.size() >= 10)
+    	{
+    		ObjectMapper objectMapper = new ObjectMapper();
+    		//Set pretty printing of json
+        	objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        	String arrayToJson = null;
+			try {
+				arrayToJson = objectMapper.writeValueAsString(TweetList);
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	System.out.println("1. Convert List of person objects to JSON :");
+        	System.out.println(arrayToJson);
+    		
+		}
+		*/
+
+    	String A = status.getUser().getScreenName();
+    	String B = status.getText();
+    	GeoLocation C = status.getGeoLocation();
+    	Tweet obj = new Tweet(A, B, C );
+    	TweetList.add(obj);
+    	System.out.println(TweetList.size());
+    	if (TweetList.size() >= 10)
+    	{
+    		ObjectMapper objectMapper = new ObjectMapper();
+    		//Set pretty printing of json
+        	objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        	String arrayToJson = null;
+			try {
+				arrayToJson = objectMapper.writeValueAsString(TweetList);
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			ObjectWriter writer = objectMapper.writer(new DefaultPrettyPrinter());
+			try {
+				writer.writeValue(new File("Lman.txt"), arrayToJson);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
+			
+        	System.out.println("1. Convert List of person objects to JSON :");
+        	System.out.println(arrayToJson);
+    		
+		}
+    	
+    	System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText() + status.getGeoLocation() );
+	
+      
     }
     
     @Override
--- CS_172_Twitter/src/Twitter.java
+++ CS_172_Twitter/src/Twitter.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import twitter4j.FilterQuery;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
@@ -5,14 +8,12 @@ import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class Twitter {
+	
+	public static List <Tweet> TweetList = new ArrayList<Tweet>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		//System.out.println(args[0]);
-		//System.out.println(args[1]);
-		//System.out.println(args[2]);
-		//System.out.println(args[3]);
 		
 		ConfigurationBuilder confBuilder = new twitter4j.conf.ConfigurationBuilder();
 		confBuilder.setOAuthConsumerKey(args[0]); 
@@ -21,11 +22,13 @@ public class Twitter {
 		confBuilder.setOAuthAccessTokenSecret(args[3]); 
 		Configuration config = confBuilder.build();
 		
+		
 		TwitterStream twitterStream = new TwitterStreamFactory(config).getInstance();
 		System.out.println(twitterStream);
 		TweetListener listener = new TweetListener(); 
 		twitterStream.addListener(listener);
 		twitterStream.sample("en");
+		twitterStream.sample();
 		
 		double[][] boundingbox = {{-124.47,24.0},{-66.56,49.3843}}; 
 		FilterQuery filter = new FilterQuery(); filter.locations(boundingbox);
